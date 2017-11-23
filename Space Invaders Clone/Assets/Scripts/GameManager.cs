883c0625e4558ac252488fbddf0414c9dfeeee58
Michael Grand <michael.grand.2@gmail.com>
2017-11-23 13:18:10 -0800

    Single enemy placed on startup. Fixed gitignore VS support.

---
+++ Space Invaders Clone/Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿namespace Assets.Scripts
+{
+    using UnityEngine;
+    using UnityEngine.Assertions;
+
+    /// <summary>
+    ///     General game code that does not belong anywhere else.
+    /// </summary>
+    public class GameManager : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform enemy1Prefab = null;
+        
+        private Transform gridPosition;
+
+        private void Start()
+        {
+            Assert.IsTrue(enemy1Prefab != null, "No type 1 enemy prefab was provided.");
+
+            GameObject gridPositionGO = GameObject.FindGameObjectWithTag(Tags.EnemyStartPosition);
+            Assert.IsTrue(gridPositionGO != null, "Could not find the grid position.");
+            gridPosition = gridPositionGO.transform;
+
+            Instantiate(enemy1Prefab, gridPosition.position, Quaternion.identity);
+        }
+    }
+}
