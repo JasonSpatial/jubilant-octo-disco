5cbdbe70b806b15639a69420ad1f831a68bb4533
Michael Grand <michael.grand.2@gmail.com>
2017-11-23 13:34:44 -0800

    The first row of enemies are now placed.

--- Space Invaders Clone/Assets/Scripts/GameManager.cs
+++ Space Invaders Clone/Assets/Scripts/GameManager.cs
@@ -10,18 +10,62 @@
     {
         [SerializeField]
         private Transform enemy1Prefab = null;
-        
-        private Transform gridPosition;
+
+        [SerializeField]
+        private float totalSpawnTime = 1;
+
+        [SerializeField]
+        private int enemiesPerRow = 11;
+
+        [SerializeField]
+        private float enemyHorizontalSpacing = 1.1f;
+
+        private float timeSinceLastEnemySpawn = 0;
+
+        private Vector3 nextEnemySpawnPosition;
+
+        private int enemiesSpawned = 0;
+
+        private bool spawningEnemies = true;
+
+        private int MaxEnemyCount
+        {
+            get { return enemiesPerRow; }
+        }
+
+        private float SpawnDelay
+        {
+            get { return totalSpawnTime/MaxEnemyCount; }
+        }
 
         private void Start()
         {
             Assert.IsTrue(enemy1Prefab != null, "No type 1 enemy prefab was provided.");
 
-            GameObject gridPositionGO = GameObject.FindGameObjectWithTag(Tags.EnemyStartPosition);
-            Assert.IsTrue(gridPositionGO != null, "Could not find the grid position.");
-            gridPosition = gridPositionGO.transform;
+            GameObject enemyStartPositionGO = GameObject.FindGameObjectWithTag(Tags.EnemyStartPosition);
+            Assert.IsTrue(enemyStartPositionGO != null, "Could not find the grid position.");
+            Transform enemyStartPosition = enemyStartPositionGO.transform;
+            nextEnemySpawnPosition = enemyStartPosition.position;
+        }
 
-            Instantiate(enemy1Prefab, gridPosition.position, Quaternion.identity);
+        private void FixedUpdate()
+        {
+            if (spawningEnemies)
+            {
+                timeSinceLastEnemySpawn += Time.fixedDeltaTime;
+                while (timeSinceLastEnemySpawn >= SpawnDelay)
+                {
+                    timeSinceLastEnemySpawn -= SpawnDelay;
+
+                    Instantiate(enemy1Prefab, nextEnemySpawnPosition, Quaternion.identity);
+                    nextEnemySpawnPosition += new Vector3(enemyHorizontalSpacing, 0, 0);
+                    enemiesSpawned++;
+                    if (enemiesSpawned >= MaxEnemyCount)
+                    {
+                        spawningEnemies = false;
+                    }
+                }
+            }
         }
     }
 }
